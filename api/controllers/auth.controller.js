3f14221666b82cbd952c416f72d42059869d9f82
WesleyAlvess <walvespereira96@gmail.com>
2024-02-05 10:33:25 -0300

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,9 +5,9 @@ import jwt from 'jsonwebtoken'
 import { validateEmail, validateSignupData, validateUserName } from '../middlewares/validation.js'
 
 
-const signup = async (req, res, next) => {
+export const signup = async (req, res, next) => {
     try {
-        const {username, email, password} = req.body
+        const { username, email, password } = req.body
 
         // Validação de email senha e espaços nos inputs
         const signupValidationResult = validateSignupData(req, res)
@@ -17,7 +17,7 @@ const signup = async (req, res, next) => {
 
         // Validação de username existente
         const userNameValidateResult = await validateUserName(req, res)
-        if(userNameValidateResult) {
+        if (userNameValidateResult) {
             return res.status(userNameValidateResult.statusCode).json(userNameValidateResult)
         }
 
@@ -36,25 +36,67 @@ const signup = async (req, res, next) => {
         })
 
         // Salvando usuário
-        await newUser.save() 
-    
-        //Token JWT
-        const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET,{ expiresIn: '1h'} )
+        await newUser.save()
 
         return res.json({
             success: true,
             statusCode: 200,
             message: "Usuário criado com sucesso",
-            token: token
         })
 
     } catch (err) {
         return res.status(500).json({
-            success:false,
+            success: false,
             statusCode: 500,
             message: "Erro interno do servidor"
         })
     }
 }
 
-export default signup
+export const signin = async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password || email === '' || password === '') {
+        return res.status(400).json({
+            success: false,
+            statusCode: 400,
+            message: "Por favor preencha todos os campos."
+        })
+    }
+    try {
+        // Verificando email
+        const validUser = await User.findOne({ email })
+        if (!validUser) {
+            return res.status(404).json({
+                success: false,
+                statusCode: 404,
+                message: "Credenciais inválidas. Por favor, verifique suas informações e tente novamente."
+            })
+        }
+
+        // Verificando password bcrypt
+        const validPassword = bcrypt.compareSync(password, validUser.password)
+        if (!validPassword) {
+            return res.status(400).json({
+                success: false,
+                statusCode: 400,
+                message: "Credenciais inválidas. Por favor, verifique suas informações e tente novamente."
+            })
+        }
+
+        //Token JWT
+        const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+        
+        return res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json({
+            success: true,
+            statusCode: 200,
+            message: "Login bem sucedido"
+        });
+        
+    } catch (err) {
+
+    }
+
+}
